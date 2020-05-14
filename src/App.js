621f552e2e0ab992a7fc2d544d16eb9043effc76
Pratham <prathamsolera@gmail.com>
2020-05-14 22:43:01 +0530

    Add animation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="container">
-      <h1 className="loading">Loading...</h1>
+      <h1 className="loading">
+        <span>L</span>
+        <span>o</span>
+        <span>a</span>
+        <span>d</span>
+        <span>i</span>
+        <span>n</span>
+        <span>g</span>
+        <span>.</span>
+        <span>.</span>
+        <span>.</span>
+      </h1>
     </div>
   );
 }
